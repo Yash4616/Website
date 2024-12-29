5664b1a0f91ab7840a23c879ac90d47378d8c941
Yash <yashgurjar9714@gmail.com>
2024-12-29 13:10:07 +0530

    first commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <div className="container mx-auto px-6 py-24 text-center relative z-10">
         <div className="mb-8">
           <img
-            src="image.jpg"
+            src="src/image.jpg"
             alt="Profile"
             className="w-32 h-32 rounded-full mx-auto border-2 border-blue-400 p-1"
           />
